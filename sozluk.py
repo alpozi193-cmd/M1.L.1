093134c0d4ef8d59b240345353ef03be52d3d7ec
alpozi193-cmd <alpozi193@gmail.com>
2026-02-09 21:47:21 +0300

    Add new slang terms to meme_dict

--- sozluk.py
+++ sozluk.py
@@ -1,12 +1,18 @@
 meme_dict = {
             "CRINGE": "Garip ya da utandırıcı bir şey",
             "LOL": "Komik bir şeye verilen cevap",
-            "SHEESH": "onaylamamak"  
+            "SHEESH": "onaylamamak", 
+            "IDK": "umursamamak",  
+            "OMG": "aman tanrım",
+            "BTW": "bu arada",
+            "FYI":  "bilgin olsun",
+            "TBH":   "dürüst olmak gerekirse",
             }
-
 word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
 
 if word in meme_dict.keys():
     print(meme_dict[word])
 else:
     print("Henüz bu kelime sözlükte yok")
+
+    
